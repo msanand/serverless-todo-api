64424daf660a705b7811ca34aecdbb9f406677d7
Anand M.S <anand.m.s@sap.com>
2018-04-14 18:56:06 +0530

    Offline support for local development and testing
    - Added offline plugin
    - Added local dynamodb
    - Included seed data for local todo table
    - Setting region in AWS SDK as it is not set by default in offline mode

--- todos/create.js
+++ todos/create.js
@@ -2,6 +2,12 @@
 
 const uuid = require('uuid');
 const AWS = require('aws-sdk');
+// set region if not set (as not set by the SDK by default). required for offline usage
+if (!AWS.config.region) {
+    AWS.config.update({
+      region: 'eu-central-1'
+    });
+}
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 
